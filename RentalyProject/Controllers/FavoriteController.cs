30f2676ff5ad43952c4ffe2da6481e263aadfbd9
i-revan <rvaniskndrov03@gmail.com>
2023-06-29 13:07:54 -0700

    added getch to home page

--- RentalyProject/Controllers/FavoriteController.cs
+++ RentalyProject/Controllers/FavoriteController.cs
@@ -15,27 +15,105 @@ namespace RentalyProject.Controllers
         private readonly AppDbContext _context;
         private readonly UserManager<AppUser> _userManager;
 
-        public FavoriteController(AppDbContext context,UserManager<AppUser> userManager)
+        public FavoriteController(AppDbContext context, UserManager<AppUser> userManager)
         {
             _context = context;
             _userManager = userManager;
         }
         public async Task<IActionResult> Index()
         {
+           
+
+            return View(await FavoriteAsync());
+        }
+        public async Task<IActionResult> AddFavorite(int? id)
+        {
+            if (id is null || id < 1) throw new BadRequestException("Id is not found");
+            Car car = await _context.Cars.Where(c => c.Id == id)
+                .Include(c => c.CarImages).FirstOrDefaultAsync();
+            if (car is null) throw new NotFoundException("There is no car has this id or it was deleted");
+            if (User.Identity.IsAuthenticated)
+            {
+                AppUser user = await _userManager.FindByNameAsync(User.Identity.Name);
+                if (user is null) throw new NotFoundException("User is not found");
+
+                bool result = await _context.FavoriteCars.AnyAsync(f => f.CarId == car.Id && f.AppUserid == user.Id);
+                if (!result)
+                {
+                    car.Like += 1;
+                    FavoriteCar favoriteCar = new FavoriteCar()
+                    {
+                        AppUserid = user.Id,
+                        CarId = car.Id,
+                        CreatedAt = DateTime.Now,
+                        RentPrice = car.RentPrice
+                    };
+                    await _context.FavoriteCars.AddAsync(favoriteCar);
+                }
+                await _context.SaveChangesAsync();
+
+            }
+            else
+            {
+                List<FavoriteCookiesVM> favorites;
+                if (Request.Cookies["Favorite"] is null)
+                {
+                    favorites = new List<FavoriteCookiesVM>();
+                    favorites.Add(new FavoriteCookiesVM
+                    {
+                        Id = car.Id
+                    });
+                }
+                else
+                {
+                    favorites = JsonConvert.DeserializeObject<List<FavoriteCookiesVM>>(Request.Cookies["Favorite"]);
+                    FavoriteCookiesVM existed = favorites.FirstOrDefault(f => f.Id == id);
+                    if (existed != null)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        favorites.Add(new FavoriteCookiesVM
+                        {
+                            Id = car.Id
+                        });
+                    }
+
+                }
+
+                string json = JsonConvert.SerializeObject(favorites);
+                Response.Cookies.Append("Favorite", json);
+            }
+
+
+            return RedirectToAction("FavoriteJson", "Favorite");
+        }
+        public async Task<IActionResult> GetFavorite()
+        {
+            var favorite = JsonConvert.DeserializeObject<List<FavoriteCookiesVM>>(Request.Cookies["Favorite"]);
+            return Json(favorite);
+        }
+        public async Task<IActionResult> FavoriteJson()
+        {
+            return Json(await FavoriteAsync());
+        }
+        public async Task<List<FavoriteItemVM>> FavoriteAsync()
+        {
             List<FavoriteItemVM> favoriteCars = new List<FavoriteItemVM>();
             if (User.Identity.IsAuthenticated)
             {
                 AppUser user = await _userManager.FindByNameAsync(User.Identity.Name);
                 if (user is null) throw new NotFoundException("User is not found");
-                List<FavoriteCar> userFavorites = await _context.FavoriteCars.Where(fc=>fc.AppUserid==user.Id)
-                    .Include(fc=>fc.Car)
-                        .ThenInclude(c=>c.CarImages.Where(ci=>ci.IsMain==true))
-                    .Include(fc=>fc.Car.Marka)
-                    .Include(fc=>fc.Car.FuelType)
-                    .Include(fc=>fc.Car.BodyType)
+                List<FavoriteCar> userFavorites = await _context.FavoriteCars.Where(fc => fc.AppUserid == user.Id)
+                    .Include(fc => fc.Car)
+                        .ThenInclude(c => c.CarImages.Where(ci => ci.IsMain == true))
+                    .Include(fc => fc.Car.Marka)
+                    .Include(fc => fc.Car.FuelType)
+                    .Include(fc => fc.Car.BodyType)
                     .ToListAsync();
-                
-                foreach(FavoriteCar item in userFavorites)
+
+                foreach (FavoriteCar item in userFavorites)
                 {
                     favoriteCars.Add(new FavoriteItemVM
                     {
@@ -92,104 +170,10 @@ namespace RentalyProject.Controllers
                     };
                     favoriteCars.Add(favoriteItemVM);
                 }
-                //foreach (var cookie in favorites)
-                //{
-                //    Car car = await _context.Cars.Include(c => c.Marka)
-                //    .Include(c => c.FuelType)
-                //    .Include(c => c.BodyType)
-                //    .Include(c => c.CarImages.Where(ci=>ci.IsMain==true)).FirstOrDefaultAsync(c => c.Id == cookie.Id);
-                //    if(car is null)
-                //    {
-                //        favorites.Remove(cookie);
-                //        continue;
-                //    }
-                //    FavoriteItemVM favoriteItemVM = new FavoriteItemVM()
-                //    {
-                //        Id = car.Id,
-                //        BodyType = car.BodyType.Name,
-                //        EngineCapacity = car.EngineCapacity,
-                //        FuelType = car.FuelType.Name,
-                //        Doors = car.Doors,
-                //        Marka = car.Marka.Name,
-                //        RentPrice = car.RentPrice,
-                //        Seats = car.Seats,
-                //        Luggage = car.Luggage,
-                //        ImageUrl = car.CarImages.FirstOrDefault().ImageUrl
-                //    };
-                //    favoriteCars.Add(favoriteItemVM);
-
-                //}
-            }
-
-            return View(favoriteCars);
-        }
-        public async Task<IActionResult> AddFavorite(int? id)
-        {
-            if (id is null || id < 1) throw new BadRequestException("Id is not found");
-            Car car = await _context.Cars.Where(c => c.Id == id)
-                .Include(c => c.CarImages).FirstOrDefaultAsync();
-            if (car is null) throw new NotFoundException("There is no car has this id or it was deleted");
-            if (User.Identity.IsAuthenticated)
-            {
-                AppUser user = await _userManager.FindByNameAsync(User.Identity.Name);
-                if (user is null) throw new NotFoundException("User is not found");
                 
-                bool result = await _context.FavoriteCars.AnyAsync(f=>f.CarId == car.Id && f.AppUserid == user.Id);
-                if (!result)
-                {
-                    car.Like += 1;
-                    FavoriteCar favoriteCar = new FavoriteCar()
-                    {
-                        AppUserid = user.Id,
-                        CarId = car.Id,
-                        CreatedAt = DateTime.Now,
-                        RentPrice = car.RentPrice
-                    };
-                    await _context.FavoriteCars.AddAsync(favoriteCar);
-                }
-                await _context.SaveChangesAsync();
-
             }
-            else
-            {
-                List<FavoriteCookiesVM> favorites;
-                if (Request.Cookies["Favorite"] is null)
-                {
-                    favorites = new List<FavoriteCookiesVM>();
-                    favorites.Add(new FavoriteCookiesVM
-                    {
-                        Id = car.Id
-                    });
-                }
-                else
-                {
-                    favorites = JsonConvert.DeserializeObject<List<FavoriteCookiesVM>>(Request.Cookies["Favorite"]);
-                    FavoriteCookiesVM existed = favorites.FirstOrDefault(f => f.Id == id);
-                    if (existed != null)
-                    {
-                        return RedirectToAction("Index", "Home");
-                    }
-                    else
-                    {
-                        favorites.Add(new FavoriteCookiesVM
-                        {
-                            Id = car.Id
-                        });
-                    }
-
-                }
-
-                string json = JsonConvert.SerializeObject(favorites);
-                Response.Cookies.Append("Favorite", json);
-            }
-            
-            
-            return RedirectToAction("Index","Home");
+            return favoriteCars;
         }
-        public async Task<IActionResult> GetFavorite()
-        {
-            var favorite = JsonConvert.DeserializeObject<List<FavoriteCookiesVM>>(Request.Cookies["Favorite"]);
-            return Json(favorite);
-        }
+
     }
 }
